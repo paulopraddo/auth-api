44055853b0cbd30466581706e6ee0d42b7aef21e
Paulo Prado <paulopraddo011@gmail.com>
2025-03-07 17:53:53 -0300

    primeiro commit

---
+++ src/test/java/com/prado/paulo/Auth/API/AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prado.paulo.Auth.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
